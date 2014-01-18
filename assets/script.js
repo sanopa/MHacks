09eda95103717c7663ca297a17915fb8fe7c6fdc
Sally McNichols <smcnicho@andrew.cmu.edu>
2014-01-18 16:07:07 -0500

    Connecting pics to objects through power of code

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,8 @@
 function myweathers($scope){
     $scope.weathers =[
-        { title: 'rainy', image:"assets/images/rainy.png", objects:[{title: 'rain coat', val: true}, {title:'snow boots',val:false}, {title:'bathing suit',val:false}, {title:'rain boots',val:true}, {title:'T-shirt',val: false}]},
-        { title: 'sunny', image:"assets/images/sunny.png", objects:[{title:'pajamas',val: false}, {title:'sunglasses',val:true}, {title:'shorts', val:false}, {title:'sneakers',val:true}, {title:'rain boots',val:false}]},
-        { title: 'cloudy', image:"assets/images/cloudy.png", objects:[{title:'sunglasses',val: false}, {title:'scarf, hat, and gloves', val:false}, {title:'pants',val:true}, {title:'shorts',val:false}, {title:'bathing suit',val: false}]},
+        { title: 'rainy', image:"assets/images/rainy.png", objects:[{title: 'rain coat', val: true,image:"assets/images/raincoat.png"}, {title:'gloves',val:false, image:"assets/images/mittens-purple.png"}, {title:'bathing suit',val:false, image:"assets/images/swimsuit.png"}, {title:'rain boots',val:true, image:"assets/images/rain-boots.png"}, {title:'T-shirt',val: false, image:"assets/images/green-tshirt.png"}]},
+        { title: 'sunny', image:"assets/images/sunny.png", objects:[{title:'pajamas',val: false, image:"assets/images/"}, {title:'sunglasses',val:true, image:"assets/images/sunglasses.png"}, {title:'shorts', val:false, image:"assets/images/white-shorts-md.png"}, {title:'sneakers',val:true, image:"assets/images/tennis_shoes.png"}, {title:'rain boots',val:false, image:"assets/images/rain-boots.png"}]},
+        { title: 'cloudy', image:"assets/images/cloudy.png", objects:[{title:'sunglasses',val: false}, {title:'scarf', val:false}, {title:'pants',val:true}, {title:'shorts',val:false}, {title:'bathing suit',val: false}]},
         { title: 'snowy', image:"assets/images/snowy.png", objects:[{title: 'bathing suit', val:false},{title:'snow boots',val: true}, {title:'coat',val: true}, {title:'pants', val: true}, {title:'scarf, hat, and gloves',val: true}]}];
     /*$scope.total=function(){
         var t = 0;

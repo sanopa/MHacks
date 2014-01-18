3529b1eaf3b1d93ca48e9d3c221b6e77af1d35ef
Sophia Anopa <sanopa@gmail.com>
2014-01-18 02:18:02 -0500

    changing js location, updating php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 # This function reads your DATABASE_URL configuration automatically set by Heroku
 # the return value is a string that will work with pg_connect
 function pg_connection_string() {
-  // we will fill this out next
+  return "dbname=d3ikol6qg14259 host=ec2-54-197-241-96.compute-1.amazonaws.com port=5432 user=ehixiwhxiwbuiz password=pPrYySOoxbIHzl_0ExDFeSEUGR sslmode=require"// we will fill this out next
 }
  
 # Establish db connection

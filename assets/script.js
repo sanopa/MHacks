d638d105b5eb904521173ebd27024deb973cf00d
Sally McNichols <smcnicho@andrew.cmu.edu>
2014-01-18 13:16:08 -0500

    Attempted to display images... Edited index & js

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,9 @@
 function myweathers($scope){
     $scope.weathers =[
-        { title: 'rainy', image:"images/rainy.png", objects:[{title: 'rain coat', val: true}, {title:'snow boots',val:false}, {title:'bathing suit',val:false}, {title:'rain boots',val:true}, {title:'T-shirt',val: false}]},
-        { title: 'sunny', image:"images/sunny.png", objects:[{title:'pajamas',val: false}, {title:'sunglasses',val:true}, {title:'shorts', val:false}, {title:'sneakers',val:true}, {title:'rain boots',val:false}]},
-        { title: 'cloudy', image:"images/cloudy.png", objects:[{title:'sunglasses',val: false}, {title:'scarf, hat, and gloves', val:false}, {title:'pants',val:true}, {title:'shorts',val:false}, {title:'bathing suit',val: false}]},
-        { title: 'snowy', image:"images/snowy.png", objects:[{title: 'bathing suit', val:false},{title:'snow boots',val: true}, {title:'coat',val: true}, {title:'pants', val: true}, {title:'scarf, hat, and gloves',val: true}]}];
+        { title: 'rainy', image:"assets/images/rainy.png", objects:[{title: 'rain coat', val: true}, {title:'snow boots',val:false}, {title:'bathing suit',val:false}, {title:'rain boots',val:true}, {title:'T-shirt',val: false}]},
+        { title: 'sunny', image:"assets/images/sunny.png", objects:[{title:'pajamas',val: false}, {title:'sunglasses',val:true}, {title:'shorts', val:false}, {title:'sneakers',val:true}, {title:'rain boots',val:false}]},
+        { title: 'cloudy', image:"assets/images/cloudy.png", objects:[{title:'sunglasses',val: false}, {title:'scarf, hat, and gloves', val:false}, {title:'pants',val:true}, {title:'shorts',val:false}, {title:'bathing suit',val: false}]},
+        { title: 'snowy', image:"assets/images/snowy.png", objects:[{title: 'bathing suit', val:false},{title:'snow boots',val: true}, {title:'coat',val: true}, {title:'pants', val: true}, {title:'scarf, hat, and gloves',val: true}]}];
     /*$scope.total=function(){
         var t = 0;
         angular.forEach($scope.services, function(s){

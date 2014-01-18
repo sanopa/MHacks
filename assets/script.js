7039b053728fc94265bb540e586fac94abb9504a
Sally McNichols <smcnicho@andrew.cmu.edu>
2014-01-18 18:34:17 -0500

    Added weather information using alert

--- assets/script.js
+++ assets/script.js
@@ -4,37 +4,57 @@ function myweathers($scope){
         { title: 'sunny', image:"assets/images/sunny.png", objects:[{title:'pajamas',val: false, image:"assets/images/pajamas.png"}, {title:'sunglasses',val:true, image:"assets/images/sunglasses.png"}, {title:'shorts', val:false, image:"assets/images/white-shorts-md.png"}, {title:'sneakers',val:true, image:"assets/images/tennis_shoes.png"}, {title:'rain boots',val:false, image:"assets/images/rain-boots.png"}]},
         { title: 'cloudy', image:"assets/images/cloudy.png", objects:[{title:'sunglasses',val: false, image:"assets/images/sunglasses.png"}, {title:'scarf', val:false, image:"assets/images/scarf.png"}, {title:'pants',val:true, image:"assets/images/trousers.png"}, {title:'shorts',val:false, image:"assets/images/white-shorts-md.png"}, {title:'bathing suit',val: false, image:"assets/images/swimsuit.png"}]},
         { title: 'snowy', image:"assets/images/snowy.png", objects:[{title: 'bathing suit', val:false, image:"assets/images/swimsuit.png"},{title:'gloves',val: true, image:"assets/images/mittens-purple.png"}, {title:'coat',val: true}, {title:'pants', val: true, image:"assets/images/trousers.png"}, {title:'scarf',val: true, image:"assets/images/scarf.png"}]}];
-    /*$scope.total=function(){
+   
+}
+
+
+
+/*
+function runWeather(){
+jQuery(document).ready(function($) {
+  $.ajax({
+  url : "http://api.wunderground.com/api/396bab011405eb77/conditions/q/autoip.jsonp",
+  dataType : "jsonp",
+  success : function(parsed_json) {
+
+
+  }
+
+
+    $scope.total=function(){
         var t = 0;
         angular.forEach($scope.services, function(s){
             if(s.selected)
                 t+=s.price;
         });
         return t;
-    };*/
+    };
 }
-/*
-var getUrl = ‘’;
+
+function runWeather(){
+var getUrl;
 jQuery(document).ready(function($) {
   $.ajax({
   url : "http://api.wunderground.com/api/396bab011405eb77/geolookup/q/autoip.json",
   dataType : "jsonp",
   success : function(parsed_json) {
   var zip = parsed_json[‘zip’];
-  getUrl = “http://api.wunderground.com/api/396bab011405eb77/conditions/q/” + zip + “.json”;
+  getUrl = "http://api.wunderground.com/api/396bab011405eb77/conditions/q/".concat(zip,“.json”);
   }
   });
 });
+}
 
 jQuery(document).ready(function($) {
   $.ajax({
   url : getUrl,
   dataType : "jsonp",
   success : function(parsed_json) {
-  var zip = parsed_json[‘zip’];
+  var z = parsed_json[‘zip’];
   var temp_f = parsed_json['current_observation']['temp_f'];
   alert("Current temperature in " + location + " is: " + temp_f);    
   }
   });
 });
+
 */

245e7e991c17c73875e14523d94589e1ccc7153b
Sophia Anopa <sanopa@gmail.com>
2014-01-18 12:32:57 -0500

    more on click checking

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 function myweathers($scope){
     $scope.weathers =[
         { title: 'rainy', objects:[{title: 'rain coat', val: true}, {title:'snow boots',val:false}, {title:'bathing suit',val:false}, {title:'rain boots',val:true}, {title:'T-shirt',val: false}]},
-        { title: 'sunny', objects:[{title:'pajamas',val: false}, {title:'sunglasses',val:true}, {title:'shorts', val:true}, {title:'sneakers',val:true}, {title:'rain boots',val:true}]},
+        { title: 'sunny', objects:[{title:'pajamas',val: false}, {title:'sunglasses',val:true}, {title:'shorts', val:false}, {title:'sneakers',val:true}, {title:'rain boots',val:false}]},
         { title: 'cloudy', objects:[{title:'sunglasses',val: false}, {title:'scarf, hat, and gloves', val:false}, {title:'pants',val:true}, {title:'shorts',val:false}, {title:'bathing suit',val: false}]},
         { title: 'snowy', objects:[{title: 'bathing suit', val:false},{title:'snow boots',val: true}, {title:'coat',val: true}, {title:'pants', val: true}, {title:'scarf, hat, and gloves',val: true}]}];
     /*$scope.total=function(){
@@ -12,10 +12,6 @@ function myweathers($scope){
         });
         return t;
     };*/
-    $scope.check=function(val){
-        if(!val)
-            alert("incorrect");
-    };
 }
 /*
 var getUrl = ‘’;
